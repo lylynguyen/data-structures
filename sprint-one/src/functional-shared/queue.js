62790659a9b915fd2ba690583ebd3900f2dfb489
lylynguyen <lylynguyen812@gmail.com>
2015-11-18 20:30:53 -0800

    implemented queue with function - shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.newVal = 1;
+  someInstance.oldVal = 1;
+
+  someInstance.storage = {}
+
+  _.extend(someInstance,queueMethods);
+
+  return someInstance;
+
 };
+
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+
+  this.storage[this.newVal] = value;
+  this.newVal++;
+}
+
+queueMethods.size = function(){
+	debugger;
+	return (this.newVal-this.oldVal) < 0 ? 0 : (this.newVal-this.oldVal) ; 
+}
+
+queueMethods.dequeue = function(){
+	var result = this.storage[this.oldVal];
+	delete this.storage[this.oldVal];;
+	this.oldVal++;
+	return result;
+}
 
