cc9a05b731b5ec16b44624fbf96c593651ae0e4f
lylynguyen <lylynguyen812@gmail.com>
2015-11-19 21:35:12 -0800

    Yahooooo! Completed tree data structures.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,15 +11,31 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+	var child = Tree(value);
+	this.children.push(child);
+
 };
 
 treeMethods.contains = function(target) {
-};
+	var result = false;
+  var recurse = function(node){
+    if(node.value === target){
+      result = true;
+    }
+    else {
+      for(var i = 0; i < node.children.length;i++){
+        recurse(node.children[i]);
+      }
+    }
+  } // closing recurse 
+  recurse(this);
+  return result;
+} // closing contains
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
