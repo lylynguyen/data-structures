afbc17848f90d00c30b4d68b2badc6564d36f4de
lylynguyen <lylynguyen812@gmail.com>
2015-11-20 20:49:39 -0800

    complete set tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
-};
+//var item = item.toString();
+	this._storage[item] = 1;
 
+}
+  
 setPrototype.contains = function(item) {
+	for(var key in this._storage){}
+	console.dir(this._storage);
+	if(this._storage[item] === undefined){
+		console.dir(this._storage);
+		return false;
+	}
+	return true;
 };
 
 setPrototype.remove = function(item) {
+	delete this._storage[item];
+
 };
 
 /*
