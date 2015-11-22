2d5fa5486915ddbaca29c59efb59759eeb1b0391
lylynguyen <lylynguyen812@gmail.com>
2015-11-21 16:40:28 -0800

    Updating hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,4 +1,4 @@
-
+ 	
 
 var HashTable = function() {
   this._limit = 8;
@@ -7,10 +7,31 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+  
+  for(var i = 0; i < bucket.length; i++){
+  	var tuple = bucket[i];
+  	if(tuple[0] === k){
+  	  tuple[1] = v;
+  	} else{
+  	  bucket[i].push([k,v]);
+  	}	
+  }
+  
+  this._storage.set(index,bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(index) || [];
+
+  for(var i; i < bucket.length; i++){
+  	var tuple = bucket[i];
+  	if(tuple[0] === k) {
+
+  	}
+  }
 };
 
 HashTable.prototype.remove = function(k) {
@@ -24,3 +45,7 @@ HashTable.prototype.remove = function(k) {
  */
 
 
+//always get index/key
+//set bucket or create 
+// iterate over tuple
+// do some action base ob what we found or havent found
