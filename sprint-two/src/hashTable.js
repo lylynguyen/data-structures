d2aae91f245cac9c5d60d628d58a79b916c71b77
lylynguyen <lylynguyen812@gmail.com>
2015-11-21 17:33:01 -0800

    Completed Hash!

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -13,29 +13,35 @@ HashTable.prototype.insert = function(k, v) {
   	var tuple = bucket[i];
   	if(tuple[0] === k){
   	  tuple[1] = v;
-  	} else{
-  	  bucket[i].push([k,v]);
-  	}	
+    }
   }
   
+  bucket.push([k,v]);
   this._storage.set(index,bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-
   var bucket = this._storage.get(index) || [];
 
-  for(var i; i < bucket.length; i++){
+  for(var i = 0; i < bucket.length;i++){
   	var tuple = bucket[i];
-  	if(tuple[0] === k) {
-
+  	if(tuple[0] === k){
+  	  return tuple[1];
   	}
   }
-};
+}
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
+
+  for(var i = 0 ; i <bucket.length; i++){
+  	var tuple = bucket[i];
+  	if(tuple[0] === k){
+  	  tuple[1] = undefined	
+  	}
+  }
 };
 
 
